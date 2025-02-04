2cdcc4f24d930b5d80bec524046293a1ef6eec02
BeamlakTesfahun <beamlakt24@gmail.com>
2025-02-04 15:31:59 +0300

    refactor: updated app module to use config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogModule } from './blog/blog.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'blog.db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Auto-create database schema (use only in dev)
+    ConfigModule.forRoot({
+      isGlobal: true, // config module is available globallys
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres', // Cast to specific DB type
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
     BlogModule,
+    AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}

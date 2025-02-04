744a9fcbb0ad7cee5acc825684b40e628e6ec049
BeamlakTesfahun <beamlakt24@gmail.com>
2025-02-04 15:36:28 +0300

    feat: implement signup and login endpoints in auth controller

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,55 @@
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async signup(username: string, email: string, password: string) {
+    const existingUser = await this.userService.findByEmail(email);
+    if (existingUser) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+
+    const salt = await bcrypt.genSalt(10); // Set a saltRounds value
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await this.userService.create({
+      username,
+      email,
+      password: hashedPassword,
+      role: 'user',
+    });
+
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new HttpException(
+        'Invalid email or password',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email, role: user.role },
+      'your-secret-key',
+      { expiresIn: '1h' },
+    );
+
+    return { token };
+  }
+}
